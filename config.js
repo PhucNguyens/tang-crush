ec8dca301ab4fb5880021cc9b9d6307ba7723b97
PhucNguyens <88439782+PhucNguyens@users.noreply.github.com>
2021-08-04 20:02:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,6 +14,6 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối mai 7h tớ qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'Okiiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
